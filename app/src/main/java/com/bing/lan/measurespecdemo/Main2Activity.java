321f8b945a6c5f29fdcd7722c3023ca8cd56cce4
lanboys <lan_bing2013@163.com>
2018-05-15 16:52:04 +0800

    添加日志类

--- app/src/main/java/com/bing/lan/measurespecdemo/Main2Activity.java
+++ app/src/main/java/com/bing/lan/measurespecdemo/Main2Activity.java
@@ -7,13 +7,15 @@ import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity {
 
+    protected final LogUtil log = LogUtil.getLogUtil(getClass(), LogUtil.LOG_VERBOSE);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
     }
 
-    public void changeSize(View view) {
+    public void changeSize(final View view) {
         Toast.makeText(this, "变大了", Toast.LENGTH_SHORT).show();
 
         ((WeChatLayout) view).startAnimator();
@@ -24,5 +26,20 @@ public class Main2Activity extends AppCompatActivity {
         //layoutParams.height += 10;
         //layoutParams.width += 10;
         //view.setLayoutParams(layoutParams);
+
+
+        // 获取的相对于父控件的值
+        // getHeight()
+        // getBottom()
+        // getTop()
+
+        view.post(new Runnable() {
+            @Override
+            public void run() {
+                log.i("run()  getHeight: " + view.getHeight());
+                log.i("run()  getBottom: " + view.getBottom());
+                log.i("run()  getTop: " + view.getTop());
+            }
+        });
     }
 }
